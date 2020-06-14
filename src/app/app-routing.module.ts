d459012b1a1fb8d67279da51ee0be75e6a3d4c68
Nathan Elvis Felippi <nathanfelippi@gmail.com>
2020-06-14 15:58:09 -0300

    projeto inicial

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AdminComponent } from './theme/layout/admin/admin.component';
+import {AuthComponent} from './theme/layout/auth/auth.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AdminComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: '/auth/signin',
+        pathMatch: 'full'
+      },
+      {
+        path: 'dashboard',
+        loadChildren: () => import('./demo/dashboard/dashboard.module').then(module => module.DashboardModule)
+      }
+    ]
+  },
+  {
+    path: '',
+    component: AuthComponent,
+    children: [
+      {
+        path: 'auth',
+        loadChildren: () => import('./demo/pages/authentication/authentication.module').then(module => module.AuthenticationModule)
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/theme/layout/admin/navigation/navigation.ts
@@ -0,0 +1,84 @@
+import {Injectable} from '@angular/core';
+
+export interface NavigationItem {
+  id: string;
+  title: string;
+  type: 'item' | 'collapse' | 'group';
+  translate?: string;
+  icon?: string;
+  hidden?: boolean;
+  url?: string;
+  classes?: string;
+  exactMatch?: boolean;
+  external?: boolean;
+  target?: boolean;
+  breadcrumbs?: boolean;
+  function?: any;
+  badge?: {
+    title?: string;
+    type?: string;
+  };
+  children?: Navigation[];
+}
+
+export interface Navigation extends NavigationItem {
+  children?: NavigationItem[];
+}
+
+const NavigationItems = [
+  {
+    id: 'forms',
+    title: 'Eventos',
+    type: 'group',
+    icon: 'feather icon-layout',
+    children: [
+      {
+        id: 'forms-element',
+        title: 'Criar um novo',
+        type: 'item',
+        url: '/forms/basic',
+        classes: 'nav-item',
+        icon: 'feather icon-file-text'
+      }
+    ]
+  },
+  {
+    id: 'avaliacao',
+    title: 'Avaliações',
+    type: 'group',
+    icon: 'feather icon-pie-chart',
+    children: [
+      {
+        id: 'charts',
+        title: 'Minhas Avaliações',
+        type: 'item',
+        url: '/charts/apex',
+        classes: 'nav-item',
+        icon: 'feather icon-heart'
+      }
+    ]
+  },
+  {
+    id: 'configuracao',
+    title: 'Configurações',
+    type: 'group',
+    icon: 'feather icon-edit',
+    children: [
+      {
+        id: 'charts',
+        title: 'Personalizar Perfil',
+        type: 'item',
+        url: '/charts/apex',
+        classes: 'nav-item',
+        icon: 'feather icon-edit'
+      }
+    ]
+  }
+];
+
+@Injectable()
+export class NavigationItem {
+  public get() {
+    return NavigationItems;
+  }
+}
